cad48fe409726182775f524ef10e5a3d0cf87728
RyandersonSilva <ryandersonsilva77@gmail.com>
2024-11-21 14:10:28 -0300

    Teste

--- main.c
+++ main.c
@@ -1,9 +1,41 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<stdlib.h>
 
-int main(){
-
-    //insira aqui seu código
-    printf("Nao implementado ainda...");
-
+    
+    int main()
+    {
+     int *vetor;
+     int t;
+     
+       scanf("%d",&t);
+       
+       vetor = (int *)malloc(t*sizeof(int));
+       
+        for( int i = 0; i < t; i++){
+         scanf("%d", &vetor[i]);
+       }
+       if(t == 0){
+           printf("[vazio]");
+       }else{
+       	
+	       printf("[");
+	       for( int i = 0; i < t; i++){
+	
+	           if(i==t-1){
+	
+	               printf("%d", vetor[i]);
+	               
+	           }else{
+	
+	               printf("%d, ", vetor[i]);
+	
+	           }
+	     
+	       }
+	    
+	      printf("]\n");
+  		}
+      free(vetor);
+ 
     return 0;
-}
+    }
